16d9c097c8e22e8bc0cfb4af4fcae4f7c2568100
George <georgevlchs20@gmail.com>
2024-11-19 11:03:59 +1100

    make hash cols nullable

--- priv/repo/migrations/20241118235113_add_hash_columns_to_users.exs
+++ priv/repo/migrations/20241118235113_add_hash_columns_to_users.exs
@@ -3,9 +3,9 @@ defmodule Onestack.Repo.Migrations.AddHashColumnsToUsers do
 
   def change do
     alter table(:users) do
-      add :bcrypt_hash, :string, null: false
-      add :argon2id_hash, :string, null: false
-      add :pkbdf2_hash, :string, null: false
+      add :bcrypt_hash, :string, null: true
+      add :argon2id_hash, :string, null: true
+      add :pkbdf2_hash, :string, null: true
     end
   end
 end
