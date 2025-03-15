156aba41ecd997350adb007c8b1f92e2c14d5a8b
George Vlachos <george@onestack.cloud>
2025-03-15 16:12:19 +0200

    migration fix

--- priv/repo/migrations/20250122010113_create_feedbacks.exs
+++ priv/repo/migrations/20250122010113_create_feedbacks.exs
@@ -12,11 +12,11 @@ defmodule Onestack.Repo.Migrations.CreateFeedbacks do
         add :user_id, references(:users, on_delete: :nilify_all)
 
         timestamps()
-
-        create index(:feedbacks, [:user_id])
-        create index(:feedbacks, [:inserted_at])
-        create index(:feedbacks, [:upvotes_count])
       end
+
+      create index(:feedbacks, [:user_id])
+      create index(:feedbacks, [:inserted_at])
+      create index(:feedbacks, [:upvotes_count])
     end
   end
 
