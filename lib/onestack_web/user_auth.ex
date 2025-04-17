43504df114557f0de136d0c8aff1b21b6ea5a397
George Vlachos <george@onestack.cloud>
2025-04-17 22:41:48 +0300

    redirect to app. if logged in

--- lib/onestack_web/user_auth.ex
+++ lib/onestack_web/user_auth.ex
@@ -299,5 +299,7 @@ defmodule OnestackWeb.UserAuth do
 
   defp maybe_store_return_to(conn), do: conn
 
-  defp signed_in_path(_conn), do: ~p"/"
+  defp signed_in_path(_conn) do
+    OnestackWeb.URLHelper.subdomain_url("app")
+  end
 end
